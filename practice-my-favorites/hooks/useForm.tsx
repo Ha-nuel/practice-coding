7a253cf0e2edcb04c0a7fc5fae9fff88263dea6a
이하늘 <wsxedc901@gmail.com>
2022-11-27 21:26:59 +0900

    useForm 훅 추가

---
+++ practice-my-favorites/hooks/useForm.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState } from 'react';
+
+interface ValidationFn {
+  (id: string, value: string): void;
+}
+
+function useForm<T>(initialState: T, validationFn?: ValidationFn) {
+  const [form, setForm] = useState(initialState);
+  const [validatedForm, setValidatedForm] = useState(initialState);
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+
+    setForm((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+
+    if (validationFn) {
+      setValidatedForm((prevState) => ({
+        ...prevState,
+        [id]: validationFn(id, value),
+      }));
+    }
+  };
+
+  return { form, validatedForm, changeHandler };
+}
+
+export default useForm;

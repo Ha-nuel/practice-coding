aa88042b69c0cf6002c241c4ead8440dd39cd009
이하늘 <wsxedc901@gmail.com>
2022-12-13 23:56:16 +0900

    Form 컨트롤러 추가

---
+++ practice-my-favorites/components/FormController.tsx
@@ -0,0 +1,29 @@
+import { currentForm } from '@/temp/formAtom';
+import { useRecoilValue } from 'recoil';
+import { FIND_ID, FIND_PW, LOGIN, REGISTER } from './constants/tabList';
+import UserIDtoFind from './UserIDtoFind';
+import UserLogin from './UserLogin';
+import UserPWtoFind from './UserPWtoFind';
+import UserRegister from './UserRegister';
+
+export default function UserFormController() {
+  const tabNumber = useRecoilValue(currentForm);
+
+  if (tabNumber === LOGIN) {
+    return <UserLogin />;
+  }
+
+  if (tabNumber === REGISTER) {
+    return <UserRegister />;
+  }
+
+  if (tabNumber === FIND_ID) {
+    return <UserIDtoFind />;
+  }
+
+  if (tabNumber === FIND_PW) {
+    return <UserPWtoFind />;
+  }
+
+  return null;
+}
